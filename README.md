a901234ab396e2e6e51846a04430f8981783ec2c
wangshijun <wangshijun2010@gmail.com>
2021-06-16 06:05:38 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coming Soon page
+
+A simple coming soon page implementation.
---
+++ tools/publish.sh
@@ -0,0 +1,18 @@
+set -e
+
+VERSION=$(cat version | awk '{$1=$1;print}')
+echo "publish version ${VERSION}"
+
+git config --local user.name "bot"
+git config --local user.email "bot@arcblock.io"
+
+make release
+
+echo "publishing blocklet..."
+npm run release
+
+echo "publishing to blocklet registry"
+blocklet config registry ${BLOCKLET_REGISTRY}
+blocklet publish --developer-sk ${ABTNODE_DEV_SK}
+
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully published\"}" ${SLACK_WEBHOOK}
